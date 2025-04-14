e3418faa1ef8291baf6bffc8a44e56c666d69ffb
ELIANEMPONTES <medeiros-eliane1@hotmail.com>
2025-04-14 08:35:44 -0300

    correção cardpostagem

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -13,11 +13,11 @@ function CardPostagem({ postagem }: CardPostagensProps) {
             <div>
                 <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
                     <img
-                        src={postagem.usuario?.foto}
+                        src="https://github.com/elianempontes.png"
                         className='h-12 rounded-full'
                         alt={postagem.usuario?.nome} />
                     <h3 className='text-lg font-bold text-center uppercase'>
-                        {postagem.usuario?.nome}
+                       Eliane Medeiros
                     </h3>
                 </div>
                 <div className='p-4 '>
